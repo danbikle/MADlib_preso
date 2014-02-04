f748c1c4d2386acfd9c901c42c2eab5f0ac54152
Dan Bikle <dan.bikle@gmail.com>
2014-02-04 06:34:18 +0000

    working towards a load script

--- load_mydata.bash
+++ load_mydata.bash
@@ -5,6 +5,11 @@
 # I use this script to load data into a table.
 
 cd ~/mlp/
+
+# Postgres needs to know where mydata.csv is.
+# It is confused by ~/tmp/
+# So I copy it to /tmp/
+cp ~/tmp/mydata.csv /tmp/
 ./psqlmad.bash -f cr_mydata.sql
 ./psqlmad.bash -f load_mydata.sql
 
--- psqlmad.bash
+++ psqlmad.bash
@@ -4,6 +4,6 @@
 
 # This is a simple wrapper script which connects me to both the madlib schema and database.
 
-PGPASSWORD=madlib psql -h 127.0.0.1 -U madlib -d madlib -p 5432 -P pager=no $@
+PGPASSWORD=madlib psql -ah 127.0.0.1 -U madlib -d madlib -p 5432 -P pager=no $@
 
 exit
